85c74190ac401fb1c3ef94986c7e423803be91d9
Grazy <karavaevvadim1999@gmail.com>
2025-06-14 23:21:51 +0300

    Prakt4_2

--- app/src/main/java/com/example/vertical_linearlayout/MainActivity.kt
+++ app/src/main/java/com/example/vertical_linearlayout/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
-        Toast.makeText(this, "OnStart!", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "OnStarts!", Toast.LENGTH_SHORT).show()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
