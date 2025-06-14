7d40acba402604e0b4c203ca769acd31ddc4bf18
Grazy-Man <104170787+Grazy-Man@users.noreply.github.com>
2025-06-15 02:04:19 +0300

    Lab_5_2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.listviewsample"
-        minSdk = 24
+        minSdk = 241
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
